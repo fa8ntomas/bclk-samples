28e3db304c5af062366f54235758bb26b973b94a
fa8ntomas <fa8ntomas@gmail.com>
2019-10-10 21:48:36 +0200

    updates

---
+++ original version (1984)/asm/map19-exec.asm
@@ -0,0 +1,57 @@
+        ; Map 19 exec
+        ; CONGRATULATIONS!
+        
+        lda Timer1
+        ora Timer2
+        bne L396A
+        
+        ; Wait timer finished, ready for next round
+        
+        inc FTYPE
+
+        lda CurrentPlayer
+        asl
+        asl
+        tax
+        ldy #$03
+        lda #$01
+L3948   sta L013C,X
+        inx
+        dey
+        bpl L3948
+        
+        jsr ResetLampsLeft
+        
+        jsr ResetLampsAll
+        
+        jsr ResetRoomsVisited
+        
+        ; Update DList
+        lda #BLANK3
+        sta L0EF1+3
+        sta L0EF1+4
+        lda #BLANK2
+        sta L0EF1+5
+        
+        ldx #$00
+        jmp GetMapStartPos
+        
+        ; Flashing fires I guess
+        
+L396A   lda RANDOM
+        sta DLCOLOR+7
+        lda RANDOM
+        sta DLCOLOR+9
+        
+        ; Update DList
+        lda #LMS+MODE2
+        sta L0EF1+3
+        lda #$A5
+        sta L0EF1+4
+        lda #$78			
+        sta L0EF1+5	
+                            ;Changes colors on last screen - $13
+        lda #$04			;Gold room
+        ldx #$00			;
+        ldy #$0A			;
+        jmp PlotChars		;---------------------------------------
---
+++ original version (1984)/asm/map19-init.asm
@@ -0,0 +1,8 @@
+        ; Map 19 init
+
+        lda #$58
+        sta Timer1
+        lda #$04
+        sta Timer2
+        rts
+            
